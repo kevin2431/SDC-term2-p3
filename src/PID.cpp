4683c06cd4acfb8f83b03fc46dc37f50c91c13fe
kevin2431 <164058292@qq.com>
2019-03-06 15:25:58 +0800

    successfully implement pid controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,13 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error=0;
+  i_error=0;
+  d_error=0;
 
 }
 
@@ -19,12 +26,16 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+  // 比例 积分 差分 三个误差计算
+  d_error = cte-p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  double error = Kp * p_error + Ki * i_error + Kd * d_error;
+  return -error;  // TODO: Add your total error calc here!
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,8 +37,20 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  //pid系数
+  double p[3]={0.552,0.00082,8.216};
+  double dp[3]={0.22,0.001,0.53};
+  int p_it=2;
+  int n=0;
+  bool direction=true;
+  bool twiddle=false;
+  double best_err=100000.0;
+  double total_err=0;
+  if(twiddle) 
+    pid.Init(p[0],p[1],p[2]);
+  else pid.Init(0.552,0.00001,8.1577);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  h.onMessage([&pid, &p, &dp, &direction, &p_it, &twiddle, &best_err, &total_err, &n](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -63,18 +75,102 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+
+          //使用pid 控制speed 即 throttle
           
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
+          //double throttle=0.24*exp(-fabs(steer_value))+0.02;
+          double throttle=0.4;
+
+          double tol=0.2;
+          int max_n=2200;
+
+          if(twiddle) {
+            if(n>100) 
+              //total_err+=pow(cte,2);
+              total_err+=fabs(cte);
+            if(n==0) pid.Init(p[0],p[1],p[2]);
+            n++;
+
+            //超出调试步数
+            if(n>max_n){
+              total_err/=((max_n-100));
+              total_err*=10000.0;
+              //正方向增长
+              if(direction){
+                if(total_err<best_err){
+                  best_err=total_err;
+                  dp[p_it]*=1.1;
+                  p_it=(p_it+1)%3;
+                  //更新p
+                  p[p_it]+=dp[p_it];
+                }else{
+                  direction=false;
+                  p[p_it]-=(2*dp[p_it]);
+                }
+              }else{
+                if(total_err<best_err){
+                  best_err=total_err;
+                  dp[p_it]*=1.1;
+                }else{
+                  p[p_it]+=dp[p_it];
+                  dp[p_it]*=0.9;
+                }
+                p_it=(p_it+1)%3;
+                p[p_it]+=dp[p_it];
+                direction=true;
+              }
+              //rest整个模拟器
+              double sum_dp=dp[0]+dp[1]+dp[2];
+              std::cout<<"err:"<<total_err<<"  best_err:"<<best_err<<std::endl;
+              if(sum_dp<tol) {
+                twiddle=false;
+                //打印最优信息
+                std::cout<<"达到最优"<<std::endl;
+
+                //暂停，等待一个按键
+              }
+              n=0;
+              total_err=0;
+              //先将角度和速度置0
+              std::string reset_msg = "42[\"reset\",{}]";
+              ws.send(reset_msg.data(), reset_msg.length(), uWS::OpCode::TEXT);
+
+            }else{
+              //正常进行下一步
+              std::cout<<"err:"<<best_err<<std::endl;
+              std::cout<<"p:"<<p[0]<<" i:"<<p[1]<<" d:"<<p[2]<<std::endl;
+              std::cout<<"dp:"<<dp[0]<<" di:"<<dp[1]<<" dd:"<<dp[2]<<std::endl;
+              std::cout<<(dp[0]+dp[1]+dp[2])<<std::endl;
+              std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }  // end "telemetry" if
+              json msgJson;
+              msgJson["steering_angle"] = steer_value;
+              msgJson["throttle"] = throttle;
+              auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+              std::cout << msg << std::endl;
+              ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
+            }
+          }else{
+            //不需要twiddle时
+            
+            // DEBUG
+            std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+                      << std::endl;
+
+            json msgJson;
+            msgJson["steering_angle"] = steer_value;
+            msgJson["throttle"] = throttle;
+            auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+            std::cout << msg << std::endl;
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          }  // end "telemetry" if
+        }
       } else {
         // Manual driving
         string msg = "42[\"manual\",{}]";
